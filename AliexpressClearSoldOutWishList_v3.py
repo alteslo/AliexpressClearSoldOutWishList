8f245855ced49d420b03714f875c5e81c6cd2e20
Teslenko Alexander <alteslo31@gmail.com>
2022-06-21 23:14:28 +0300

    rename misc.py, add description for class

--- AliexpressClearSoldOutWishList_v3.py
+++ AliexpressClearSoldOutWishList_v3.py
@@ -1,5 +1,5 @@
 from config import load_config
-from misc import Cursor
+from cursor import Cursor
 
 config = load_config('./.env')
 
@@ -7,7 +7,8 @@ MY_USER = config.aliexpress.login
 MY_PASSWORD = config.aliexpress.password
 
 
-cursor = Cursor(MY_USER, MY_PASSWORD, headless=False)
-login = cursor.login()
-if login:
-    cursor.kill_them_all()
+if __name__ == '__main__':
+    cursor = Cursor(MY_USER, MY_PASSWORD, headless=False)
+    login = cursor.login()
+    if login:
+        cursor.kill_them_all()
--- cursor.py
+++ cursor.py
@@ -1,10 +1,10 @@
 from time import sleep
 
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.common import exceptions
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,6 +13,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class Cursor:
+    """
+    Cursor object that allows you to interact with the Aliexpress wishlist
+    """
     ALI_URL = 'https://login.aliexpress.ru/'
     ALI_WISH_URL = 'https://my.aliexpress.ru/wishlist/wish_list_product_list.htm?currentGroupId=0'
 
@@ -32,6 +35,9 @@ class Cursor:
         return driver
 
     def login(self):
+        """
+        Authorize and returns True if success
+        """
         driver = Cursor.__get_driver(self.headless)
         # self.driver = driver
         driver.get(Cursor.ALI_URL)
@@ -41,7 +47,9 @@ class Cursor:
             wait.until(
                 EC.presence_of_element_located((By.ID, "fm-login-id"))
             )
-        finally:
+        except Exception:
+            return False
+        else:
             login = driver.find_element(By.ID, 'fm-login-id')
             login.send_keys(self.user)
 
@@ -55,11 +63,8 @@ class Cursor:
             self.driver = driver
         return True
 
-    def kill_them_all(self):
-        sleep(5)
-        driver = self.driver
-        driver.get(Cursor.ALI_WISH_URL)
-
+    @staticmethod
+    def _load_all_items(driver):
         while True:
             try:
                 button_load_more = driver.find_element(By.CLASS_NAME, 'Wishlist_ButtonLoadMore__loadMoreBtn__zjmf8')
@@ -67,25 +72,26 @@ class Cursor:
             except exceptions.NoSuchElementException:
                 break
 
+    def kill_them_all(self):
+        """
+        Completely clears the wishlist
+        """
+        sleep(5)
+        driver = self.driver
+        driver.get(Cursor.ALI_WISH_URL)
+
+        self._load_all_items(driver)
+
         links = driver.find_elements_by_class_name('Wishlist_ActionBlock__delete__1mups')
 
         print(f'Количество предметов в списке жеданий {len(links)}')
         actions = ActionChains(driver)
         for link in tqdm(links):
-            # actions.move_to_element(link)
+            actions.move_to_element(link)
             actions.click(link)
 
-
-        
-        # for i in tqdm(range(1, 51, 1)):  # 50 pages for wishlist
-        #     # you may set group id for u wishlist
-        #     driver.get(Cursor.ALI_WISH_URL + str(i))
-        #     links = driver.find_elements(By.XPATH, (
-        #         '/html/body/div[4]/div[2]/div/div[2]/div[2]/ul/li[@class="product sold-out util-clearfix js-product"]/div['
-        #         '2]/p/a'))
-
-        #     urls = [link.get_attribute("href") for link in links]
-        
-        # for u in tqdm(urls):
-        #     print(u)
-        #     driver.get(u)
+    def bury_only_the_dead(self):
+        """
+        Clear wishlist from...
+        """
+        pass
