609cf6b495db51ccf1688cb0dfba943dc67ae8cc
Teslenko A.A.(work) <alteslo31@gmail.com>
2022-06-22 17:36:34 +0300

    refactoring

--- AliexpressClearSoldOutWishList_v3.py
+++ AliexpressClearSoldOutWishList_v3.py
@@ -9,6 +9,6 @@ MY_PASSWORD = config.aliexpress.password
 
 if __name__ == '__main__':
     cursor = Cursor(MY_USER, MY_PASSWORD, headless=False)
-    login = cursor.login()
-    if login:
-        cursor.kill_them_all()
+    # auth = cursor.login()
+    auth = False
+    cursor.kill_them_all(auth)
--- cursor.py
+++ cursor.py
@@ -24,22 +24,26 @@ class Cursor:
         self.password = password
         self.headless = headless
 
-    @staticmethod
-    def __get_driver(headless):
+    @property
+    def driver(self):
         if headless:
             options = ChromeOptions()
             options.add_argument("--headless")
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+            driver = webdriver.Chrome(
+                service=Service(ChromeDriverManager().install()),
+                options=options
+            )
             return driver
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install())
+        )
         return driver
 
     def login(self):
         """
         Authorize and returns True if success
         """
-        driver = Cursor.__get_driver(self.headless)
-        # self.driver = driver
+        driver = self.driver
         driver.get(Cursor.ALI_URL)
 
         try:
@@ -49,46 +53,51 @@ class Cursor:
             )
         except Exception:
             return False
+
         else:
             login = driver.find_element(By.ID, 'fm-login-id')
             login.send_keys(self.user)
-
             password = driver.find_element(By.ID, 'fm-login-password')
             password.send_keys(self.password)
 
             driver.find_element(
-                By.XPATH, '/html/body/div[1]/div/div[3]/div/div[1]/div[3]/div/button'
+                By.XPATH,
+                '/html/body/div[1]/div/div[3]/div/div[1]/div[3]/div/button'
             ).click()
             driver.switch_to.default_content()
-            self.driver = driver
         return True
 
     @staticmethod
     def _load_all_items(driver):
         while True:
             try:
-                button_load_more = driver.find_element(By.CLASS_NAME, 'Wishlist_ButtonLoadMore__loadMoreBtn__zjmf8')
+                button_load_more = driver.find_element(
+                    By.CLASS_NAME,
+                    'Wishlist_ButtonLoadMore__loadMoreBtn__zjmf8'
+                )
                 button_load_more.click()
             except exceptions.NoSuchElementException:
                 break
 
-    def kill_them_all(self):
+    def kill_them_all(self, auth):
         """
         Completely clears the wishlist
         """
-        sleep(5)
         driver = self.driver
         driver.get(Cursor.ALI_WISH_URL)
+        if auth:
+            self._load_all_items(driver)
 
-        self._load_all_items(driver)
+            links = driver.find_elements_by_class_name('Wishlist_ActionBlock__delete__1mups')
 
-        links = driver.find_elements_by_class_name('Wishlist_ActionBlock__delete__1mups')
-
-        print(f'Количество предметов в списке жеданий {len(links)}')
-        actions = ActionChains(driver)
-        for link in tqdm(links):
-            actions.move_to_element(link)
-            actions.click(link)
+            print(f'Количество предметов в списке жеданий {len(links)}')
+            actions = ActionChains(driver)
+            for link in tqdm(links):
+                actions.move_to_element(link)
+                actions.click(link)
+                actions.perform()
+        else:
+            raise Exception('Please check your credentials')
 
     def bury_only_the_dead(self):
         """
